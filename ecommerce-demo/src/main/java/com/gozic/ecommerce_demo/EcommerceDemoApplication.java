f69d21024526b6f2a112c6cca3acdc9784b31056
Khanh Phan <nguyenhiu2k2@gmail.com>
2025-05-14 09:08:59 +0700

    first init

---
+++ ecommerce-demo/src/main/java/com/gozic/ecommerce_demo/EcommerceDemoApplication.java
@@ -0,0 +1,23 @@
+package com.gozic.ecommerce_demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EcommerceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
